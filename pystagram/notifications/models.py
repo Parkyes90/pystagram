421496d31a4ea00e7c00c4915f08b799419bd55d
parkyes90 <parkyes90@gmail.com>
2018-05-21 14:17:27 +0900

    create notification models and setting notification admin

--- pystagram/notifications/models.py
+++ pystagram/notifications/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from pystagram.users.models import User
+from pystagram.images.models import Image, TimeStampedModel
 
-# Create your models here.
+
+class Notification(TimeStampedModel):
+
+    TYPE_CHOICES = (
+        ('like', 'Like'),
+        ('comment', 'Comment'),
+        ('follow', 'Follow')
+    )
+
+    creator = models.ForeignKey(
+        User, related_name='creator', on_delete=models.CASCADE
+    )
+    to = models.ForeignKey(
+        User, related_name='to', on_delete=models.CASCADE
+    )
+    notification_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    image = models.ForeignKey(
+        Image, null=True, blank=True, on_delete=models.CASCADE
+    )
