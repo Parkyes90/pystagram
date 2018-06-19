c2f7b6a049260e642d076af53c9327eb00bb59c2
parkyes90 <parkyes90@gmail.com>
2018-06-19 20:03:58 +0900

    add like action

--- pystagram/images/views.py
+++ pystagram/images/views.py
@@ -37,7 +37,11 @@ class Images(APIView):
             image_list, key=lambda img: img.created_at, reverse=True
         )
 
-        serializer = serializers.ImageSerializer(sorted_list, many=True)
+        serializer = serializers.ImageSerializer(
+            sorted_list,
+            many=True,
+            context={'request': request}
+        )
 
         return Response(serializer.data)
 
@@ -232,7 +236,10 @@ class ImageDetail(APIView):
         except models.Image.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        serializer = serializers.ImageSerializer(image)
+        serializer = serializers.ImageSerializer(
+            image,
+            context={'request': request}
+        )
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
